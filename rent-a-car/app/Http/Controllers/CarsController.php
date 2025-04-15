35390cd29697a9bc9b511e58560d9366266e7a26
Natouille04 <nathan.gouveia2008@gmail.com>
2025-04-15 13:57:06 +0200

    Created vehicules view

--- rent-a-car/app/Http/Controllers/CarsController.php
+++ rent-a-car/app/Http/Controllers/CarsController.php
@@ -28,10 +28,40 @@ class carsController extends Controller
         ->join('vehicule_type as vt', 'vt.id', '=', 'v.vehicule_type_id')
         ->join('vehicule_photo as vp', 'vp.vehicule_id', '=', 'v.id')
         ->groupBy('v.id')
+        ->limit(6)
         ->get();
 
         return view("home", [
             "cars" => $cars
         ]);
+    }
+
+    public function allVehicules() {
+        $cars = DB::table('vehicule as v')
+        ->select(
+            'v.id',
+            'v.brand',
+            'v.model',
+            DB::raw('MAX(vp.image_url) as img'),
+            'v.year',
+            'v.price_per_day',
+            'v.doors',
+            'v.fuel_type',
+            'v.air_conditioning',
+            'v.seats',
+            'v.transmission',
+            'vt.name as type',
+            DB::raw("GROUP_CONCAT(e.name SEPARATOR ', ') as equipements")
+        )
+        ->join('vehicule_equipment as ve', 've.vehicule_id', '=', 'v.id')
+        ->join('equipment as e', 've.equipment_id', '=', 'e.id')
+        ->join('vehicule_type as vt', 'vt.id', '=', 'v.vehicule_type_id')
+        ->join('vehicule_photo as vp', 'vp.vehicule_id', '=', 'v.id')
+        ->groupBy('v.id')
+        ->get();
+
+        return view("vehicules", [
+            "cars" => $cars
+        ]);  
     }
 }
--- rent-a-car/routes/web.php
+++ rent-a-car/routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\carsController;
 
 Route::get('/', [carsController::class, 'index'])->name('home');
+Route::get('/vehicules', [carsController::class, 'allVehicules'])->name('home');
